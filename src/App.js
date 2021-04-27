190eccf48bace8b64d79c90e53af1c4cdf81f686
ganesh <ganeshneerati7@gmail.com>
2021-04-27 17:24:02 +0530

    integrattion front end with fake store api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [value, Setvalue] = useState();
+
+  const [data, setData] = useState([]);
+
+  const fetchProduct = async () => {
+    const data = await axios.get(
+      `https://fakestoreapi.com/products/category/${value}`
+    );
+    console.log(data);
+    setData(data.data);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Search for product</h1>
+      <div className="search_conainer">
+        <label>Enter product category:</label>
+        <input
+          type="text"
+          placeholder="enter product category"
+          onChange={(e) => Setvalue(e.target.value)}
+        />
+        <button onClick={fetchProduct}> Search</button>
+
+        {data &&
+          data.map((d) => (
+            <div className="data">
+              <span>Title</span> <p>{d.title}</p>
+              <span>Description</span>
+              <p>{d.description}</p>
+              <span>Price</span>
+              <p>{d.price}</p>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
